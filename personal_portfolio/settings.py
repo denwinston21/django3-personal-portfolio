053669e73accd3635e6d1d848469adddf4c516af
kristallnacht <fdk.21@mail.ru>
2022-06-11 09:57:20 +0000

    Remove old stuff

--- personal_portfolio/settings.py
+++ personal_portfolio/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-pro8u5g-_@@9fstg9-k3!2vi^)w$@*5&1u%o3*gm)r7!@*eoeh'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['kristallnacht.pythonanywhere.com']
 
 
 # Application definition
@@ -118,6 +118,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATIC_ROOT = BASE_DIR / 'static'
+
 
 MEDIA_URL = 'media/'
 
